b871b0934a873b2e2ce3e60a1d26a6056f2c859e
Mizanur Rahman <mizanurrahaman020195@gmail.com>
2021-12-30 00:17:27 +0600

    area create

---
+++ src/OnlineVotingSystem/OnlineVotingSystem.Web/WebModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+
+namespace OnlineVotingSystem.Web
+{
+    public class WebModule : Module
+    {
+        public WebModule()
+        {
+
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
